6491c7b8ca40db0affe0ed68b7a2d291bdb529b4
ElKna <yyt012@gmail.com>
2023-07-29 03:16:46 +0900

    Fix: Add pprint

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-### python-boto3-practice
+## python-boto3-practice
 
 파이썬에서 boto3를 사용하면서 기록을 저장하는 용도의 리포
--- get-caller-identity/boto3-practice.py
+++ get-caller-identity/boto3-practice.py
@@ -1,5 +1,12 @@
 import boto3
+from pprint import pprint
 
 client = boto3.client("sts")
 res = client.get_caller_identity()
-print(res)
+pprint(res)
+print("\n", res["UserId"], res["Arn"])
+
+if res ["Arn"].endswith("root"):
+    print("루트 계정을 사용중입니다.")
+else:
+    print("루트 계정을 사용하고있지 않습니다.")
