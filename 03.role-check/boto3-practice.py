41c7399877e92b67391580d52d7dad4c5ac32787
ElKna <yyt012@gmail.com>
2023-07-30 03:04:33 +0900

    Fix: Role checker python file add condition

--- 03.role-check/boto3-practice.py
+++ 03.role-check/boto3-practice.py
@@ -1,8 +1,23 @@
 import boto3
 from pprint import pprint
 
+from datetime import datetime, timezone, timedelta
+
+now = datetime.now(timezone.utc)
+
 client = boto3.client("iam")
 roles = client.list_roles()["Roles"]
 for role in roles:
     # pprint(role)
-    print(role["RoleName"])
+    # print(role["RoleName"])
+    name = role["RoleName"]
+    r = client.get_role(RoleName=name)["Role"]
+    # pprint(r)
+    last_used_date = r["RoleLastUsed"].get("LastUsedDate")
+    print(name, last_used_date)
+    if not last_used_date:
+        print(name, "400일 내 사용 기록 없음")
+    elif (now - last_used_date) > timedelta(days=90):
+        print(name, f"90일 내 사용 기록 없음 | {last_used_date}")
+    else:
+        print(name, f"90일 내 사용 기록 있음 : {last_used_date}")
